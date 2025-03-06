c51172b8b0b78ba63c892bfb47fda1d43771d625
TheEgglet <woodstuffthings@gmail.com>
2025-03-06 20:09:55 +0100

    untrack some stuff

--- EggScript/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using EggScript;
-
-internal class Program
-{
-	private static string Root => $"{AppContext.BaseDirectory}/../../..";
-	private static string Path => $"{Root}/script.egg";
-
-	private static void Main() => Eggscript.Execute(Path);
-}
