269b69f2c1d8ec61cbd88f49568379ceeaba3bae
TheEgglet <woodstuffthings@gmail.com>
2025-03-07 13:14:41 +0100

    add OperatorNode

---
+++ EggScript/Parsing/Nodes/Expression/OperatorNode.cs
@@ -0,0 +1,20 @@
+﻿namespace EggScript.Parsing.Nodes.Expression;
+
+/// <summary>
+/// Represents a binary operator node.
+/// </summary>
+public class OperatorNode(IExpressionNode left, string op, IExpressionNode right) : IExpressionNode
+{
+	/// <summary>
+	/// The kind of operator, like addition or multiplication.
+	/// </summary>
+	public string Operator { get; } = op;
+	/// <summary>
+	/// The left expression.
+	/// </summary>
+	public IExpressionNode Left { get; } = left;
+	/// <summary>
+	/// The right expression.
+	/// </summary>
+	public IExpressionNode Right { get; } = right;
+}
