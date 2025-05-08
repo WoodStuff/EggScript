c065812f341d1ed63c3a2b44cb4703b19c9b3d9d
TheEgglet <woodstuffthings@gmail.com>
2025-05-08 18:22:20 +0200

    add eggscript runner

---
+++ EggScript.Runner/Program.cs
@@ -0,0 +1,27 @@
+﻿using EggScript.Exceptions;
+
+namespace EggScript.Runner;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		if (args.Length != 1)
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine("Incorrect usage! Please specify a path to the EggScript file.");
+			return;
+		}
+
+		try
+		{
+			Eggscript.Execute(args[0]);
+		}
+		catch (EggScriptException e)
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine($"Error: {e.Message}");
+		}
+		Console.ReadKey();
+	}
+}
